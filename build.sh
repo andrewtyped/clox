e7329b9f267530d7dd713d0626185b257f9d5785
Andrew Barger <asbarg01@gmail.com>
2022-07-09 20:09:39 +0000

    Build script links files

--- build.sh
+++ build.sh
@@ -4,10 +4,24 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 SRC_FILE="$SCRIPT_DIR/src/main.c"
-OUT_DIR="$SCRIPT_DIR/bin"
-OUT_FILE="$OUT_DIR/clox"
+SRC_DIR="$SCRIPT_DIR/src"
+BIN_DIR="$SCRIPT_DIR/bin"
+OBJ_DIR="$SCRIPT_DIR/obj"
+OUT_FILE="$BIN_DIR/clox"
 
-mkdir -p $OUT_DIR
+mkdir -p $BIN_DIR
+mkdir -p $OBJ_DIR
 
+FILES=("memory" \
+       "chunk" \
+       "main")
 
-gcc $SRC_FILE -o $OUT_FILE
+for f in ${FILES[@]}; do
+    gcc -c "$SRC_DIR/$f.c" -o "$OBJ_DIR/$f.o" 
+done
+
+OBJFILES=$(for f in ${FILES[@]}; do echo "$OBJ_DIR/$f.o"; done)
+
+echo $OBJFILES
+
+gcc $OBJFILES -o $OUT_FILE
