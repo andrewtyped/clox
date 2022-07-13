5a68578c1673e34177301f2a5a0ca2c82a821969
Andrew Barger <asbarg01@gmail.com>
2022-07-13 03:34:16 +0000

    placeholder tests

--- test/main.c
+++ test/main.c
@@ -1,20 +1,17 @@
-#include "../src/common.h"
-#include "../src/chunk.h"
 #include <unity.h>
 
 void setUp() {}
 void tearDown() {}
 
-void test_Unity(void)
+void test_1(void)
 {
     TEST_ASSERT_EQUAL_UINT8(1,1);
-    TEST_ASSERT_EQUAL_UINT8(1,2);
 }
 
 int main(int argc, const char* argv[]) {
     UNITY_BEGIN();
 
-    RUN_TEST(test_Unity);
+    RUN_TEST(test_1);
     
     return UNITY_END();
 }
