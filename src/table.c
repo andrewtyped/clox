f6da27c025a5bf52b7039a31761ee501463c66ac
Andrew Barger <asbarg01@gmail.com>
2022-11-13 04:02:34 +0000

    Implement standard operations for hash tables

--- src/table.c
+++ src/table.c
@@ -6,6 +6,8 @@
 #include "table.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table* table) {
     table->count = 0;
     table->capacity = 0;
@@ -15,4 +17,118 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
     FREE_ARRAY(Entry, table->entries, table->capacity);
     initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+    Entry* tombstone = NULL;
+
+    for(;;) {
+        Entry* entry = &entries[index];
+        if (entry->key == NULL) {
+            if (IS_NIL(entry->value)) {
+                //We didn't find the key. Return the tombstone, if any, so the caller can overwrite it with a new entry if they want to.
+                return tombstone != NULL ? tombstone: entry;
+            } else {
+                if (tombstone == NULL) {
+                    //We found a tombstone.
+                    tombstone = entry;
+                }
+            }
+        } else if (entry->key == key) {
+            //We found the key.
+            return entry;
+        }
+
+        index = (index + 1) % capacity;
+    }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+    Entry* entries = ALLOCATE(Entry, capacity);
+
+    //Initialize the newly allocated hash table capacity
+    for(int i = 0; i < capacity; i++) {
+        entries[i].key = NULL;
+        entries[i].value = NIL_VAL;
+    }
+
+    //copy existing entries over to the new, larger table.
+    table->count = 0;
+    for(int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if (entry->key == NULL) {
+            //Skip empty entries and tombstones.
+            continue;
+        }
+
+        Entry* dest = findEntry(entries, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+        table->count++;
+    }
+
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    table->entries = entries;
+    table->capacity = capacity;
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+    if (table->count == 0) {
+        return false;
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+
+    if (entry->key == NULL) {
+        return false;
+    }
+
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+    return true;
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    if (table->count == 0) {
+        return false;
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+
+    if (entry->key == NULL) {
+        return false;
+    }
+
+    *value = entry->value;
+    return true;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    if (table->count + 1 > table->capacity + TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustCapacity(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+
+    bool isNewKey = entry->key == NULL;
+
+    //If the IS_NIL check fails, the entry we found is a tombstone, and is already accounted for in the array's capacity. We don't need to increment the count;
+    if (isNewKey && IS_NIL(entry->value)) {
+        table->count++;
+    }
+
+    entry->key = key;
+    entry->value = value;
+    return isNewKey;
+}
+
+void tableAddAll(Table* from, Table* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* entry = &from->entries[i];
+        if (entry->key != NULL) {
+            tableSet(to, entry->key, entry->value);
+        }
+    }
 }
--- src/table.h
+++ src/table.h
@@ -17,5 +17,8 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTable(Table* table);
-
+bool tableDelete(Table* table, ObjString* key);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+void tableAddAll(Table* from, Table* to);
 #endif
