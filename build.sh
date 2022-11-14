29d70ef912d574e712954e69f9acdb59c0031ee4
Andrew Barger <asbarg01@gmail.com>
2022-11-14 03:18:08 +0000

    play around with compiling in debug mode... doesn't seem to work yet

--- build.sh
+++ build.sh
@@ -8,8 +8,15 @@ SRC_DIR="$SCRIPT_DIR/src"
 BUILD_DIR="$SCRIPT_DIR/build"
 OBJ_DIR="$SCRIPT_DIR/obj"
 OUT_FILE="$BIN_DIR/clox"
+BUILD_TYPE="Debug"
+
+if [ "$1" != "" ]
+then 
+    BUILD_TYPE=$1
+fi
 
 mkdir -p $BUILD_DIR
 
+#cmake -S "$SCRIPT_DIR" -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake -S "$SCRIPT_DIR" -B $BUILD_DIR
 cmake --build "$BUILD_DIR"
