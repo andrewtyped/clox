690225878fa865d95358e0008d2bceb58b7438c1
Andrew Barger <asbarg01@gmail.com>
2022-11-17 03:33:29 +0000

    Support for loops

--- src/vm.c
+++ src/vm.c
@@ -146,6 +146,7 @@ static InterpretResult run() {
                 case OP_SET_LOCAL: {
                     uint8_t slot = READ_BYTE();
                     vm.stack[slot] = peek(0);
+                    break;
                 }
                 case OP_GET_GLOBAL: {
                     ObjString* name = READ_STRING();
